16c2d380ce1ef5ef672179fb4720e2ab14d6b01e
PacoPanadero18 <126406188+PacoPanadero18@users.noreply.github.com>
2023-03-04 10:30:15 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php
+require("conexion.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $contraseña = $_POST["contraseña"];
+
+    $sql = "select * from usuario where nombre='" . $usuario . "' AND pass='" . $contraseña . "'";
+
+    $resultado = mysqli_query($con, $sql);
+
+    $row = mysqli_fetch_array($resultado);
+
+    if ($row["tipo_usuario"] == "1") {
+        $_SESSION["usuario"] = $usuario;
+        header("location:usuario.php");
+    } elseif ($row["tipo_usuario"] == "0") {
+        $_SESSION["usuario"] = $usuario;
+        header("location:admin.php");
+    } else {
+        echo "El usuario o contraseña son incorrectos";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<body>
+    <center>
+        <h1>Login</h1>
+        <br><br><br>
+        <form action="#" method="POST">
+            <div>
+                Usuario: <input type="text" name="usuario" required>
+            </div>
+            <br>
+            <div>
+                Contraseña: <input type="password" name="contraseña" required>
+            </div>
+            <br>
+            <div>
+                <input type="submit" value="login">
+            </div>
+            <br>
+        </form>
+    </center>
+</body>
+</head>
+
+</html>
